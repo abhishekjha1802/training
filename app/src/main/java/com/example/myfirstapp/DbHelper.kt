280d54dfa530af2da1e67e55a2132a542ff9a3b0
Abhishek Kumar <abhishek.jha@toothsi.in>
2022-01-11 01:47:39 -0800

    Added Form Persistence using shared preferences and  Saved data using sqlite

---
+++ app/src/main/java/com/example/myfirstapp/DbHelper.kt
@@ -0,0 +1,18 @@
+package com.example.myfirstapp
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import java.security.AccessControlContext
+
+class DbHelper(context: Context): SQLiteOpenHelper(context,"UserDb",null,1) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL("Create Table Users(id Integer PRIMARY KEY AUTOINCREMENT,name Text,gender Text,address Text,mobile_no Text,dob Text)")
+
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+
+    }
+
+}
