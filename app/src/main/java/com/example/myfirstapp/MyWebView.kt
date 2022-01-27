eb95be50814c59e076c46fbb9edd20000f8d6d0b
Abhishek Kumar <abhishek.jha@toothsi.in>
2022-01-26 21:12:19 -0800

    Added Communication with webview

--- app/src/main/java/com/example/myfirstapp/MyWebView.kt
+++ app/src/main/java/com/example/myfirstapp/MyWebView.kt
@@ -1,17 +1,28 @@
 package com.example.myfirstapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JavascriptInterface
 import android.webkit.WebView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MyWebView : AppCompatActivity() {
     private lateinit var myWebView: WebView
+    private lateinit var mEditText:EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_web_view)
+        mEditText=findViewById(R.id.editTextAV)
 
         webViewSetup()
+
+        findViewById<Button>(R.id.sendToWV).setOnClickListener{
+            sendDataToWebView()
+        }
     }
 
     private fun webViewSetup(){
@@ -19,6 +30,22 @@ class MyWebView : AppCompatActivity() {
 
         var webSettings=myWebView.settings
         webSettings.javaScriptEnabled=true
+        myWebView.addJavascriptInterface(JSBridge(this,findViewById(R.id.editTextAV)),"JSBridge")
         myWebView.loadUrl("file:///android_asset/index.html")
     }
+
+    class JSBridge(val context: Context, val editTextInput: EditText){
+        @JavascriptInterface
+        fun showMessageInNative(message:String){
+            Toast.makeText(context,"Data Send To Native", Toast.LENGTH_LONG).show()
+            editTextInput.setText(message)
+        }
+    }
+
+    private fun sendDataToWebView(){
+        myWebView.evaluateJavascript(
+            "javascript: " +"updateFromNative(\"" + mEditText.text + "\")",
+            null)
+    }
 }
+
