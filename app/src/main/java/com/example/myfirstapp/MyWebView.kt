c201a149d30537453acaa2e3aa52093484ef534d
Abhishek Kumar <abhishek.jha@toothsi.in>
2022-01-27 21:07:54 -0800

    Fetched device info in webview

--- app/src/main/java/com/example/myfirstapp/MyWebView.kt
+++ app/src/main/java/com/example/myfirstapp/MyWebView.kt
@@ -1,28 +1,41 @@
 package com.example.myfirstapp
 
 import android.content.Context
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import android.webkit.JsResult
+
+import android.webkit.WebChromeClient
+
+
+
 
 class MyWebView : AppCompatActivity() {
     private lateinit var myWebView: WebView
     private lateinit var mEditText:EditText
+    private lateinit var brand:String
+    private lateinit var model:String
+    private lateinit var id:String
+    private lateinit var manufacture:String
+    private lateinit var sdk:String
+    private lateinit var user:String
+    private lateinit var type:String
+    private lateinit var base:String
+    private lateinit var versionCode:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_web_view)
-        mEditText=findViewById(R.id.editTextAV)
 
         webViewSetup()
 
-        findViewById<Button>(R.id.sendToWV).setOnClickListener{
-            sendDataToWebView()
-        }
     }
 
     private fun webViewSetup(){
@@ -30,22 +43,52 @@ class MyWebView : AppCompatActivity() {
 
         var webSettings=myWebView.settings
         webSettings.javaScriptEnabled=true
-        myWebView.addJavascriptInterface(JSBridge(this,findViewById(R.id.editTextAV)),"JSBridge")
+        myWebView.addJavascriptInterface(JSBridge(this),"JSBridge")
         myWebView.loadUrl("file:///android_asset/index.html")
+
+
+        myWebView.webChromeClient = object : WebChromeClient() {
+            //Other methods for your WebChromeClient here, if needed..
+            override fun onJsAlert(
+                view: WebView,
+                url: String,
+                message: String,
+                result: JsResult
+            ): Boolean {
+                return super.onJsAlert(view, url, message, result)
+            }
+        }
     }
 
-    class JSBridge(val context: Context, val editTextInput: EditText){
+    inner class JSBridge(val context: Context){
         @JavascriptInterface
-        fun showMessageInNative(message:String){
-            Toast.makeText(context,"Data Send To Native", Toast.LENGTH_LONG).show()
-            editTextInput.setText(message)
+        fun sendRequest(){
+            Toast.makeText(context,"Request Received", Toast.LENGTH_LONG).show()
+            getDeviceInfo()
+
+
+            runOnUiThread(Runnable {
+                sendDataToWebView()
+            })
+
         }
     }
 
     private fun sendDataToWebView(){
-        myWebView.evaluateJavascript(
-            "javascript: " +"updateFromNative(\"" + mEditText.text + "\")",
-            null)
+        myWebView.evaluateJavascript("javascript: getResult(\"$brand\",\"$model\",\"$id\",\"$sdk\",\"$manufacture\",\"$user\",\"$type\",\"$base\",\"$versionCode\");",null);
+
+    }
+
+    private fun getDeviceInfo(){
+        brand= "Brand: ${Build.BRAND}  "
+        model="Model: ${Build.MODEL} "
+        id="ID: ${Build.ID} "
+        sdk="SDK: ${Build.VERSION.SDK_INT} "
+        manufacture="Manufacture: ${Build.MANUFACTURER} "
+        user="User: ${Build.USER} "
+        type="Type: ${Build.TYPE} "
+        base="Base: ${Build.VERSION_CODES.BASE} "
+        versionCode="Version Code: ${Build.VERSION.RELEASE}"
     }
 }
 
