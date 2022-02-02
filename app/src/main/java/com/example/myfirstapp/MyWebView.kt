e910405bbc53c5822af3f00427bb68a5bd8b19c2
Abhishek Kumar <abhishek.jha@toothsi.in>
2022-02-02 00:08:03 -0800

    Added Signin in webview through app

--- app/src/main/java/com/example/myfirstapp/MyWebView.kt
+++ app/src/main/java/com/example/myfirstapp/MyWebView.kt
@@ -1,18 +1,22 @@
 package com.example.myfirstapp
 
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
-import android.webkit.JavascriptInterface
-import android.webkit.WebView
+import android.view.View
+import android.webkit.*
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import android.webkit.JsResult
 
-import android.webkit.WebChromeClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+
 
 
 
@@ -29,23 +33,39 @@ class MyWebView : AppCompatActivity() {
     private lateinit var type:String
     private lateinit var base:String
     private lateinit var versionCode:String
+    private lateinit var firebaseAuth:FirebaseAuth
+    private lateinit var webViewClient:WebViewClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_web_view)
 
         webViewSetup()
+        println(CookieManager.getInstance().getCookie("stackoverflow.com"))
 
     }
+
+
+
 
     private fun webViewSetup(){
         myWebView=findViewById(R.id.myWebView)
 
         var webSettings=myWebView.settings
         webSettings.javaScriptEnabled=true
+        webSettings.setUserAgentString("com.example.myfirstapp")
+
+
+
+        myWebView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+        }
+
         myWebView.addJavascriptInterface(JSBridge(this),"JSBridge")
         myWebView.loadUrl("file:///android_asset/index.html")
-
 
         myWebView.webChromeClient = object : WebChromeClient() {
             //Other methods for your WebChromeClient here, if needed..
@@ -72,11 +92,34 @@ class MyWebView : AppCompatActivity() {
             })
 
         }
+
+        @JavascriptInterface
+        fun signIn(){
+            checkUser()
+            runOnUiThread(Runnable {
+                myWebView.evaluateJavascript("javascript: signInResult(\"${firebaseAuth.currentUser?.email.toString()}\");",null);
+            })
+        }
     }
 
     private fun sendDataToWebView(){
         myWebView.evaluateJavascript("javascript: getResult(\"$brand\",\"$model\",\"$id\",\"$sdk\",\"$manufacture\",\"$user\",\"$type\",\"$base\",\"$versionCode\");",null);
 
+    }
+
+
+    private fun checkUser(){
+        firebaseAuth=FirebaseAuth.getInstance()
+        var firebaseUser=firebaseAuth.currentUser
+        if(firebaseUser==null)
+        {
+            var intent= Intent(this,SignIn::class.java)
+            startActivity(intent)
+        }
+        else
+        {
+
+        }
     }
 
     private fun getDeviceInfo(){
@@ -92,9 +135,10 @@ class MyWebView : AppCompatActivity() {
     }
 
 
+    var doubleBackToExitPressedOnce = false
     override fun onBackPressed() {
         if (myWebView.canGoBack()) {
-            myWebView.goBack()
+                myWebView.goBack()
         } else {
             super.onBackPressed()
         }
